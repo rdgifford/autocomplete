dec539c8ed42b0eea42b34aeff0dea4fb6e06f0c
rdgifford <robbie.gifford@gmail.com>
2017-03-21 16:43:38 -0700

    Push logic in jQuery back into React to take full advantage of its structure and rendering capabilities

--- src/main.js
+++ src/main.js
@@ -3,18 +3,98 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import _ from 'lodash';
 
+// Configurations
+const url = 'http://api.bing.com/osjson.aspx?JsonType=callback&JsonCallback=?';
+const suggestionsLimit = 10;
+const suggestionDebounceWait = 200;
+
+class Suggestion extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <option value={this.props.value}/>
+        )
+    }
+}
+
 class AutoComplete extends React.Component {
+    constructor(props) {
+        super(props)
+        this.updateSuggestions = _.debounce(this.updateSuggestions, suggestionDebounceWait)
+        this.state = {
+            suggestions: [],
+            placeholderVisible: '',
+            placeholderInvisible: '',
+        }
+    }
+    resetSuggestions() {
+        // if(_.some(this.state, stateValue => stateValue > 0)) {
+            this.setState({
+                suggestions: [],
+                placeholderVisible: '',
+                placeholderInvisible: '',
+            })
+        // }
+    }
+    updateSuggestions(event) {
+        console.log('inside wrapper', event);
+        let targetValue = event.target.value;
+
+        this.resetSuggestions();
+        $.getJSON(url, {
+            query: targetValue
+        }, data => {
+            let suggestions = data[1];
+            let suggestion, placeholder;
+
+            // if(targetValue === $('input.autoComplete').val()) {
+                if(suggestions.length > 0) {
+                    placeholder = suggestions.shift();
+                    suggestions = suggestions.slice(0, suggestionsLimit);
+
+                    this.setState({
+                        suggestions: suggestions,
+                        placeholderVisible: placeholder.slice(targetValue.length),
+                        placeholderInvisible: placeholder.slice(0, targetValue.length),
+                    })
+                }
+            // }
+        })
+    }
     render() {
         return (
             <div className="autoComplete">
                 <div className="logo">Autocomplete</div>
                 <div className="textInput">
                     <div className="autoComplete placeHolder">
-                        <span className="invisible"></span>
-                        <span className="visible"></span>
+                        <span className="invisible">{this.state.placeholderInvisible}</span>
+                        <span className="visible">{this.state.placeholderVisible}</span>
                     </div>
-                    <input className="autoComplete" size="50" list="d1"></input>
-                    <datalist id="d1"></datalist>
+                    <input
+                        className="autoComplete"
+                        size="50"
+                        list="d1"
+                        onKeyDown={this.resetSuggestions.bind(this)}
+                        onKeyUp={
+                            event => {
+                                event.persist()
+                                this.updateSuggestions.call(this, event)
+                            }
+                        }
+                    ></input>
+                    <datalist id="d1"> { 
+                        this.state.suggestions.map((suggestion, i) => {
+                            return (
+                                <Suggestion
+                                    key={i}
+                                    value={suggestion}
+                                />
+                            )
+                        })
+                    } </datalist>
                 </div>
             </div>
         );
@@ -22,45 +102,5 @@ class AutoComplete extends React.Component {
 }
 
 $().ready(() => {
-    const url = 'http://api.bing.com/osjson.aspx?JsonType=callback&JsonCallback=?';
-    let limit = 10;
-    let container = $('.container')[0];
-
-    ReactDOM.render(<AutoComplete />, container);
-
-    let resetAutocomplete = event => {
-        $('span.invisible').text('');
-        $('span.visible').text('');
-        $('#d1').empty();
-    }
-
-    let updateAutocomplete = event => {
-		let targetValue = event.target.value;
-
-        resetAutocomplete();
-		$('span.invisible').text(targetValue);
-
-        $.getJSON(url, {
-            query: targetValue
-        }, data => {
-            let suggestions = data[1];
-            let currLimit, suggestion, option;
-            if(targetValue === $('input.autoComplete').val()) {
-
-                if(suggestions.length > 0) {
-                    $('span.visible').text(suggestions.shift().slice(targetValue.length));
-
-                    currLimit = data[1].length < 10 ? data[1].length : limit;
-                    for(let i = 0; i < currLimit; i++) {
-                        suggestion = data[1][i];
-                        option = $('<option>');
-                        $('#d1').append(option.attr('value', suggestion));
-                    }
-                }
-            }
-        })
-    };
-
-    $('.autoComplete').keydown(resetAutocomplete)
-    $('input.autoComplete').keyup(_.debounce(updateAutocomplete, 200));
+    ReactDOM.render(<AutoComplete />, $('.container')[0]);
 });
